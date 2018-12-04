300dc685b760a60283f6d15addcdf431452a0431
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2018-12-03 21:24:46 -0500

    Day2

---
+++ Day 2/Day2.py
@@ -0,0 +1,20 @@
+# Part 1
+from collections import Counter
+a, b = 0, 0
+for w in open('Data').read().splitlines():
+    counts = Counter(Counter(w).values())
+    if 2 in counts:
+        a+=1
+    if 3 in counts:
+        b+=1
+
+print(a*b)
+
+#Part 2
+data = open('Data').read().splitlines()
+for x in data:
+    for y in data:
+        diff = [i for i,j in zip(x,y) if i == j]
+        if len(y)-len(diff) == 1:
+            print("".join(diff))
+            break
